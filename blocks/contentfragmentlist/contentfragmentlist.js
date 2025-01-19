b4ec11ccacf66607b20da5450ff303596048d8bb
raulugarte <63106469+raulugarte@users.noreply.github.com>
2025-01-19 12:10:17 +0100

    Update contentfragmentlist.js

--- blocks/contentfragmentlist/contentfragmentlist.js
+++ blocks/contentfragmentlist/contentfragmentlist.js
@@ -67,7 +67,7 @@ async function getCategories(persistedQuery, isUE) {
     /*const items = json?.data?.offerList?.items || [] */
     /* const items = json?.data?.blog-by-slug?.items || [] */
     const itemsrug = json?.data?.blog-by-slug?.items || []
-    const test=1;
+    const test=itemsrug.toString();
 
     return items.map((item) => {
         /*const imageUrl = getImageUrl(item.image, isUE);*/
